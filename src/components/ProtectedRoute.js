04bf5dee406e4ba1ac0b88dc721a20657ff16b0b
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-07-13 00:00:43 +0300

    delete route

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { Navigate, Route } from 'react-router-dom';
 
 const ProtectedRoute = ({ element: Component, ...props }) => {
-  <Route>
-    return props.isLoggedIn ? <Component {...props} /> : <Navigate to="/sigh-in" replace />;
-  </Route>;
+  return props.isLoggedIn ? <Component {...props} /> : <Navigate to="/sigh-in" replace />;
 };
 
 export default ProtectedRoute;
