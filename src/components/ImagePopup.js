24011cb6879fdee12e57977f6fd64aa862548ae6
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-07-13 19:49:03 +0300

    refactor: fix template string imape zoom link

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,7 +2,7 @@ function ImagePopup({ card, onClose, isPopupOpen }) {
   return (
     <div className={`popup  ${isPopupOpen ? 'popup_opened' : ''}`} onClick={onClose}>
       <div className="popup__zoom-container">
-        <img src={`${card.link}`} alt={`${card.name}`} className="popup__zoom-image" />
+        <img src={card.link} alt={`${card.name}`} className="popup__zoom-image" />
         <h2 className="popup__zoom-image-text">{`${card.name}`}</h2>
         <button
           type="button"
