2e6aca76fc7ac2e496ae4e53388e5906a865baee
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-07-15 15:57:54 +0300

    refactor popupImage

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -6,7 +6,7 @@ function ImagePopup({ card, onClose, isPopupOpen }) {
   };
 
   return (
-    <div className={`popup  ${isPopupOpen ? 'popup_opened' : ''}`} onClick={onClose}>
+    <div className={`popup  ${isPopupOpen ? 'popup_opened' : ''}`} onClick={handleWindowCloseClick}>
       <div className="popup__zoom-container">
         <img src={card.link} alt={`${card.name}`} className="popup__zoom-image" />
         <h2 className="popup__zoom-image-text">{`${card.name}`}</h2>
@@ -14,7 +14,7 @@ function ImagePopup({ card, onClose, isPopupOpen }) {
           type="button"
           aria-label="Закрыть"
           className="popup__close-icon popup__close-zoom"
-          onClick={handleWindowCloseClick}
+          onClick={onClose}
         />
       </div>
     </div>
